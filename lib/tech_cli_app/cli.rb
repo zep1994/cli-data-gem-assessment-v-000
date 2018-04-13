b4a0abc7016442a1d17c66126e7cbce0ea750c3e
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-04-13 14:31:24 +0000

    Create list news method to test with fake data in bin

--- lib/tech_cli_app/cli.rb
+++ lib/tech_cli_app/cli.rb
@@ -1,7 +1,15 @@
 class TechCliApp::CLI
 
   def call
-    puts "hello"
+    puts "The Most Recent Tech News is:"
+    list_tech_news
+  end
+
+  def list_tech_news
+      puts <<-DOC
+      1. VR!
+      2. AR!
+        DOC
   end
 
 end
