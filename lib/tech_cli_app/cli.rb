1adfff96a602984af85ad2bd7866b47f0e4bb800
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-04-13 14:40:39 +0000

    added menu with exit and options for 1 & 2

--- lib/tech_cli_app/cli.rb
+++ lib/tech_cli_app/cli.rb
@@ -1,15 +1,34 @@
 class TechCliApp::CLI
 
   def call
-    puts "The Most Recent Tech News is:"
     list_tech_news
+    menu
   end
 
   def list_tech_news
       puts <<-DOC
+      puts "The Most Recent Tech News is:"
       1. VR!
       2. AR!
         DOC
+  end
+
+  def menu
+    puts "Enter the number of the article you would like to read or type exit to quit:"
+    input = nil
+    while input != "exit"
+      input = gets.strip.downcase
+    case input
+    when "1"
+      puts "info 1"
+    when "2"
+      puts "info 2"
+    end
+  end
+end
+
+  def goodbye
+    puts "See you Later!"
   end
 
 end
