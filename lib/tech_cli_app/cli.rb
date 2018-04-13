9d5cc906ac3c98b2e1a22c31bf5171fdef8dbef1
Thomas Wesley Matlock <thomasmatlock1994@gmail.com>
2018-04-13 14:53:07 +0000

    add list, menu, and goodbye methods to cli.rb

--- lib/tech_cli_app/cli.rb
+++ lib/tech_cli_app/cli.rb
@@ -3,6 +3,7 @@ class TechCliApp::CLI
   def call
     list_tech_news
     menu
+    goodbye
   end
 
   def list_tech_news
@@ -14,7 +15,7 @@ class TechCliApp::CLI
   end
 
   def menu
-    puts "Enter the number of the article you would like to read or type exit to quit:"
+    puts "Enter the number of the article you would like to read, type list to relist news or type exit to quit:"
     input = nil
     while input != "exit"
       input = gets.strip.downcase
@@ -23,6 +24,10 @@ class TechCliApp::CLI
       puts "info 1"
     when "2"
       puts "info 2"
+    when "list"
+      list_tech_news
+    else
+      puts "Not sure what you want:/ type list or exit"
     end
   end
 end
